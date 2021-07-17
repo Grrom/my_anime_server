098b6d0ca911db9d0540b5655c978776b3154eca
JeromeLalunio <josejeromelalunio@gmail.com>
2021-07-17 13:38:18 +0800

    setup filesystem read

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,33 @@
 const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req: any, res: any) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
-    console.log(req);
-});
-
-server.listen(port, hostname, () => {
+http.createServer((request: any, response: any) => {
+    console.log("got a request");
+    switch (request.url) {
+        case "/":
+            response.writeHead(200, { "Content-Type": "application/json" });
+            response.write(animeList());
+            break;
+        default:
+            response.writeHead(200, { "Content-Type": "text/html" });
+            response.write("not home");
+    }
+    response.end();
+}).listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+const animeList = (): string => {
+    let animes: { [k: string]: string } = {};
+    fs.readdirSync("./anime").forEach((animeName: string) => {
+        if (animeName !== ".keep")
+            animes[animeName] = fs.readdirSync(`./anime/${animeName}`);
+    });
+    return JSON.stringify(animes);
+}
+
+

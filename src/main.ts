6eed3ce960c37723b3b933a6c5fd30aeb5c73be3
JeromeLalunio <josejeromelalunio@gmail.com>
2021-07-22 05:03:34 +0800

    add params to video reqeust

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 
 const http = require('http');
+const url = require('url');
 
 const { readdirSync, stat, createReadStream } = require('fs');
 const { promisify } = require("util");
@@ -12,7 +13,7 @@ const port = 3000;
 
 http.createServer((request: any, response: any) => {
 
-    switch (request.url) {
+    switch (url.parse(request.url, true).pathname) {
         case "/anime-list":
             response.writeHead(200, {
                 "Content-Type": "application/json",
@@ -23,31 +24,44 @@ http.createServer((request: any, response: any) => {
             response.end();
             break;
         case "/video":
-            const sampleVideo = "./anime/somali/01.mp4";
+            const query = url.parse(request.url, true).query;
+            const requestVideo = `./anime/${query.anime}/${query.episode}`;
 
-            stat(sampleVideo, (err: any, stats: any) => {
+            stat(requestVideo, (err: any, stats: any) => {
                 if (err !== null && err.code === "ENOENT") {
                     response.writeHead(404)
                 }
                 const fileSize = stats.size
 
-                console.log(fileSize)
                 const head = {
                     'Content-Length': fileSize,
                     'Content-Type': 'video/mp4',
                     "Access-Control-Allow-Origin": "http://localhost:8080",
                     "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE",
                 }
-
                 response.writeHead(200, head);
-                createReadStream(sampleVideo).pipe(response);
+                createReadStream(requestVideo).pipe(response);
             })
-
-
             break;
+        case "/testing":
+            console.log(url.parse(request.url, true).query)
+            response.writeHead(
+                200, {
+                "Access-Control-Allow-Origin": "http://localhost:8080",
+                "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE",
+            });
+            response.write(JSON.stringify(url.parse(request.url, true).query))
+            response.end()
+            break
         default:
-            response.writeHead(200, { "Content-Type": "text/html" });
-            response.write("not home");
+            response.writeHead(
+                200, {
+                "Access-Control-Allow-Origin": "http://localhost:8080",
+                "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE",
+            });
+            console.log(url.parse(request.url, true).pathname)
+            response.write(JSON.stringify("hello"))
+            response.end()
     }
 }).listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);

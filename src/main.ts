823c90683df7d337b1d2dd8f44d9bc54151a955c
JeromeLalunio <josejeromelalunio@gmail.com>
2021-07-22 11:45:59 +0800

    support partal content responses

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,9 @@
+import { statSync } from "fs";
 
 const http = require('http');
 const url = require('url');
 
 const { readdirSync, stat, createReadStream } = require('fs');
-const { promisify } = require("util");
-const { pipeline } = require("stream");
-
-const fileInfo = promisify(stat);
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -26,33 +23,47 @@ http.createServer((request: any, response: any) => {
         case "/video":
             const query = url.parse(request.url, true).query;
             const requestVideo = `./anime/${query.anime}/${query.episode}`;
+            const videoSize = statSync(requestVideo).size;
 
-            stat(requestVideo, (err: any, stats: any) => {
-                if (err !== null && err.code === "ENOENT") {
-                    response.writeHead(404)
-                }
-                const fileSize = stats.size
+            const range = request.headers.range;
+            if (range) {
+                const CHUNK_SIZE = 10 ** 6;
+                const start = Number(range.replace(/\D/g, ""));
+                const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
 
-                const head = {
-                    'Content-Length': fileSize,
-                    'Content-Type': 'video/mp4',
+                response.writeHead(206, {
+                    "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+                    "Accept-Ranges": "bytes",
+                    "Content-Length": end - start + 1,
+                    "Content-Type": "video/mp4",
                     "Access-Control-Allow-Origin": "http://localhost:8080",
+                    "Access-Control-Allow-Headers": "Range",
                     "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE",
-                }
-                response.writeHead(200, head);
-                createReadStream(requestVideo).pipe(response);
-            })
+                });
+
+                createReadStream(requestVideo, { start, end }).pipe(response)
+
+            } else {
+                stat(requestVideo, (err: any, stats: any) => {
+                    if (err !== null && err.code === "ENOENT") {
+                        response.writeHead(404)
+                    }
+                    const fileSize = stats.size
+
+                    response.writeHead(206, {
+                        'Content-Length': fileSize,
+                        'Content-Type': 'video/mp4',
+                        "Accept-Ranges": "bytes",
+                        "Access-Control-Allow-Headers": "Range",
+                        "Access-Control-Allow-Origin": "http://localhost:8080",
+                        "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE",
+                    });
+
+                    createReadStream(requestVideo).pipe(response);
+                })
+            }
+
             break;
-        case "/testing":
-            console.log(url.parse(request.url, true).query)
-            response.writeHead(
-                200, {
-                "Access-Control-Allow-Origin": "http://localhost:8080",
-                "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE",
-            });
-            response.write(JSON.stringify(url.parse(request.url, true).query))
-            response.end()
-            break
         default:
             response.writeHead(
                 200, {

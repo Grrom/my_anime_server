0ebae81dd149c3de3a6bd5fd24c26086aae71497
JeromeLalunio <josejeromelalunio@gmail.com>
2021-07-24 06:05:52 +0800

    handle pre-flight request

--- src/anime_server.ts
+++ src/anime_server.ts
@@ -8,12 +8,20 @@ export class AnimeServer {
 
     constructor() {
         createServer(function (request: any, response: any) {
+
             switch (request.method) {
                 case "GET":
                     getRoutes(request, response);
                     break;
                 case "POST":
                     postRoutes(request, response);
+                case "OPTIONS":
+                    response.writeHead(200, {
+                        "Access-Control-Allow-Headers": "Content-Type, Accept",
+                        "Access-Control-Allow-Origin": "http://localhost:8080",
+                        "Access-Control-Allow-Methods": "POST",
+                    })
+                    response.end()
                     break
             }
         }).listen(this.port, this.hostname, () => {
